2d32e8982cd899f89d3d3d4f84b0d65fc2a6cb45
OwalaOARL <112147607+OwalaOARL@users.noreply.github.com>
2025-02-13 19:58:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/intro/intro";
+import Skills from "./components/skills/skills";
+import Works from "./components/Works/works";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
+      <Skills/>
+      <Works/>
     </div>
   );
 }
 
 export default App;
-
