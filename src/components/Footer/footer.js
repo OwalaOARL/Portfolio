f81490aca887838442046d387df8f9f43dcd60bd
OwalaOARL <112147607+OwalaOARL@users.noreply.github.com>
2025-02-15 19:16:10 +0530

    Add files via upload

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const footer = () => {
+  return (
+    <footer className='footer'>
+      © 2024 Rashini Lakshitha | Designed & Developed with ❤️ and passion for web & software innovation.
+    </footer>
+  );
+}
+
+export default footer;
